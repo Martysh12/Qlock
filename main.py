1ad5c4195b6c0b0fd5bf452e6fc1877172c716b0
Martin K <martyshkon@gmail.com>
2022-12-12 16:54:49 +0200

    Allow setting window flags from context menu

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from datetime import datetime
 
 from PyQt6.QtCore import QPoint, Qt, QTimer
-from PyQt6.QtGui import QAction, QColor, QCursor, QFont, QPainter, QPainterPath
+from PyQt6.QtGui import QAction, QActionGroup, QColor, QCursor, QFont, QPainter, QPainterPath
 from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QMenu
 
 from qlock_config_manager import load_config, save_config
@@ -14,11 +14,16 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.locked = False
+        self.is_always_on_top = False
+        self.is_like_other_windows = True
+        self.is_always_on_bottom = False
+
         self.setup_window()
         self.setup_widgets()
         self.setup_config(not_renderer=True)
 
-        self.locked = False
+        self.do_set_flags(1)
 
         self.renderer = QlockRenderer(self.config)
 
@@ -51,20 +56,40 @@ class MainWindow(QMainWindow):
 
         self.menu = QMenu()
 
-        settingsAction = QAction("Settings", self)
+        settingsAction = self.menu.addAction("Settings")
         settingsAction.triggered.connect(self.open_settings)
 
-        lockAction = QAction("Lock", self)
+        lockAction = self.menu.addAction("Lock")
         lockAction.setCheckable(True)
         lockAction.triggered.connect(self.lock)
 
-        exitAction = QAction("Exit", self)
-        exitAction.triggered.connect(self.close)
+        topBottomActionGroup = QActionGroup(self)
+        topBottomActionGroup.setExclusive(True)
 
-        self.menu.addAction(settingsAction)
-        self.menu.addAction(lockAction)
         self.menu.addSeparator()
-        self.menu.addAction(exitAction)
+
+        AOTAction = self.menu.addAction("Always On Top")
+        AOTAction.setCheckable(True)
+        AOTAction.setChecked(self.is_always_on_top)
+        AOTAction.triggered.connect(lambda: self.do_set_flags(0))
+        topBottomActionGroup.addAction(AOTAction)
+
+        LOWAction = self.menu.addAction("Like Other Windows")
+        LOWAction.setCheckable(True)
+        LOWAction.setChecked(self.is_like_other_windows)
+        LOWAction.triggered.connect(lambda: self.do_set_flags(1))
+        topBottomActionGroup.addAction(LOWAction)
+
+        AOBAction = self.menu.addAction("Always On Bottom")
+        AOBAction.setCheckable(True)
+        AOBAction.setChecked(self.is_always_on_bottom)
+        AOBAction.triggered.connect(lambda: self.do_set_flags(2))
+        topBottomActionGroup.addAction(AOBAction)
+
+        self.menu.addSeparator()
+
+        exitAction = self.menu.addAction("Exit")
+        exitAction.triggered.connect(self.close)
 
         self.oldPos = self.pos()
 
@@ -131,6 +156,27 @@ class MainWindow(QMainWindow):
     def lock(self, checked):
         self.locked = checked
 
+    def do_set_flags(self, what):
+        new_flags = Qt.WindowType.FramelessWindowHint #| Qt.WindowType.Tool
+
+        self.is_always_on_top = False
+        self.is_like_other_windows = False
+        self.is_always_on_bottom = False
+
+        if what == 0:
+            new_flags |= Qt.WindowType.WindowStaysOnTopHint
+            self.is_always_on_top = True
+        elif what == 1:
+            new_flags |= 0 # None to add.
+            self.is_like_other_windows = True
+        elif what == 2:
+            new_flags |= Qt.WindowType.WindowStaysOnBottomHint
+            self.is_always_on_bottom = True
+
+        # QT always adds 1 to the window flags. I'm presuming it's just adding Qt.WindowType.Window.
+        if self.windowFlags() != (new_flags | Qt.WindowType.Window):
+            self.setWindowFlags(new_flags)
+            self.show()
 
 app = QApplication(sys.argv)
 
