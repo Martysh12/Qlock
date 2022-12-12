bcdb6ff91f2746f36436a7124f92c6c8b8c88106
Martin K <martyshkon@gmail.com>
2022-12-12 02:06:54 +0200

    Generate default config when nonexistant

--- qlock_config_manager.py
+++ qlock_config_manager.py
@@ -1,10 +1,27 @@
 import json
 
+def generate_default_config():
+    with open("conf.json", "w") as f:
+        return json.dump(
+            {
+                "text_color": [255, 255, 255],
+                "background_color": [0, 0, 0],
+                "opacity": 0.67,
+                "font_face": "Noto Sans",
+                "font_size": 14,
+                "size": [539, 364]
+            },
+            f
+        )
 
 def load_config():
-    with open("conf.json", "r") as f:
-        return json.load(f)
+    try:
+        with open("conf.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        generate_default_config()
 
+        return load_config()
 
 def save_config(config):
     with open("conf.json", "w") as f:
